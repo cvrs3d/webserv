47d03602e8ff60170dc61a56cb799020bdb688ce
cvrs3d <ev.ustinov03@gmail.com>
2025-11-18 10:18:12 +0100

    0.1: Server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cvrs3d/webserv
+
+go 1.25.2
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	multiplexer := http.NewServeMux()
+
+	if multiplexer == nil {
+		log.Fatal("Allocation error!!!")
+	}
+
+	server := http.Server {
+		Addr: ":8080",
+		Handler: multiplexer,
+	}
+	
+	defer server.Close()
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal("Error: ", err)
+	}
+}
