e00be82238f5b527c954af48fe307d7e60907733
cvrs3d <ev.ustinov03@gmail.com>
2025-11-18 13:38:51 +0100

    Decoupling

--- main.go
+++ main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+
 func main() {
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	multiplexer := http.NewServeMux()
 
-	multiplexer.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	multiplexer.Handle("/app/", apiCfg.middlewareMetrics(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
 	assetsHandler := http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets")))
-	multiplexer.Handle("/app/assets", assetsHandler)
-	multiplexer.Handle("/app/assets/", assetsHandler)
-	multiplexer.HandleFunc("/healthz", healthHandler)
+	multiplexer.Handle("/app/assets", apiCfg.middlewareMetrics(assetsHandler))
+	multiplexer.Handle("/app/assets/", apiCfg.middlewareMetrics(assetsHandler))
+	multiplexer.HandleFunc("GET /api/healthz", healthHandler)
+	multiplexer.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	multiplexer.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
 
 	if multiplexer == nil {
 		log.Fatal("Allocation error!!!")
---
+++ middleware.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	value := cfg.fileserverHits.Load()
+	w.Header().Set("Content-type", "text/html")
+	message := fmt.Sprintf(`
+		<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+		</html>`, value)
+	io.WriteString(w, message)
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
